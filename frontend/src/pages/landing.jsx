8b6a03b55230433ee7cf65463836a93d7823b859
RandSohail <randsohail98@gmail.com>
2024-10-21 14:37:54 +0300

    file structure, add eslint, prettier, react router dom

---
+++ frontend/src/pages/landing.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Landing extends React.Component {
+  render() {
+    return (
+      <div className={{ border: "1px solid red" }}>
+        <p>This is the Landing page</p>
+      </div>
+    );
+  }
+}
